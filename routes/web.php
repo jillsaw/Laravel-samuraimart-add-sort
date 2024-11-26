66bd1f05a09d48a04c3e4ef3a21b3c15a956ec1b
jillsaw <uiharu4510471@gmail.com>
2024-11-26 22:07:40 +0900

    アマゾン風アプリ33章

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CheckoutController;
 
 
 /*
@@ -54,5 +55,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::post('users/carts', 'store')->name('carts.store');
         Route::delete('users/carts', 'destroy')->name('carts.destroy');
 
+    });
+
+    Route::controller(CheckoutController::class)->group(function () {
+        Route::get('checkout', 'index')->name('checkout.index');
+        Route::post('checkout', 'store')->name('checkout.store');
+        Route::get('checkout/success', 'success')->name('checkout.success');
+  
     });
 });
